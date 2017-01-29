a9e9aa662d2b912d1843873d41898e7148124c76
monzelb <monzelb@gmail.com>
2017-01-28 22:17:31 -0800

    added gif and jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,34 @@
-// sanity check
-console.log("app.js linked.");
+$( document ).ready(function() {
+    console.log( "ready!" );
+/*
+    $(".options").hover(function(){
+    $(this).css("background-color", "rgba(255, 255, 255, 0.27)");
+    }, function(){
+    $(this).css("background-color", "rgba(255, 255, 255, 0.15)");
+
+});*/
+
+$(".right").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "rgba(255, 255, 255, 0.15)");
+});
+
+
+$(".options").hover(function(){
+	$(this).addClass("show-gif");
+}, function(){
+	$(this).removeClass("show-gif");
+
+	
+
+});
+  
+
+
+
+    
+
+
+
+});
