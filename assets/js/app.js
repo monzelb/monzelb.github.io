e438414e3eefdf9224f7ae88aa60dc5993adbcb2
monzelb <monzelb@gmail.com>
2017-01-29 23:05:50 -0800

    designed projects page

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,23 +12,22 @@ var projects = [
 	{name: "Portfolio",
 	picture: "assets/imgs/portfolio.png",
 	link: "https://monzelb.github.io/",
-	description: "An ongoing adventure in my exploration of html, CSS and javascript. Check here for my latest projects."
+	description: "An ongoing adventure and exploration of html, CSS and javascript. Check here for my latest projects."
 	},
 	{name: "Karma",
 	picture:"assets/imgs/karma.png",
 	link: "https://github.com/monzelb/startup-landing-page-clone",
-	description: "A mockup of a fictional company's website.",
+	description: "A mockup of a fictional company's website using only html and css.",
 	},
 	{name: "Jquery streetfighter",
 	picture: "assets/imgs/street-fighter.png",
 	link: "https://github.com/monzelb/jquery-streetfighter",
 	description:"One of my first endeavors using Jquery to make an interactive webpage."
-	},
-	{picture: "assets/imgs/street-fighter.png"}
+	}
 ]
 
 for(var i=0; i<projects.length; i++){
-	$(".row").append(`<div><a class= '` + `projects col-md-4'` + ` href='` + projects[i].link + `'><img src=` + projects[i].picture + `></a></div>`);
+	$(".row").append(`<div class='` + `projects col-md-4'` + `><h2>` + projects[i].name + `</h2><h5>` + projects[i].description + `</h5><a class='` + `frame'` + ` href='` + projects[i].link + `'><img class= '` + `thumbs'` + `src='` + projects[i].picture + `'></a></div>`);
 	
 }
 
