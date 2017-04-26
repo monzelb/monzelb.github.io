383e1ffff56f95c367ec594fb1d6a15cd131315f
monzelb <monzelb@gmail.com>
2017-04-26 12:32:23 -0700

    improved appearances

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,7 +33,19 @@ var projects = [
 // for(var i=0; i<projects.length; i++){
 // 	$(".row").append(`<div class='` + `projects col-md-4'` + `><h2>` + projects[i].name + `</h2><h5>` + projects[i].description + `</h5><a class='` + `fram'` + ` href='` + projects[i].link + `'><img class= '` + `thumbs'` + `src='` + projects[i].picture + `'></a></div><br>`);
 // }
+$('.top-buttons1 ').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
 
+$('.mid-buttons1 ').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
 
 $(".pink").hover(function(){
     $(this).css("background-color", "mistyrose");
@@ -41,18 +53,45 @@ $(".pink").hover(function(){
     $(this).css("background-color", "rgba(255, 255, 255, 0.15)");
 });
 
-$(".mid-buttons").mouseover(function(){
+
+$(".mid-buttons1 ").mouseover(function(){
     $(this).css("color", "black");
 });
-$(".mid-buttons").mouseout(function(){
+$(".mid-buttons1 ").mouseout(function(){
     $(this).css("color", "white");
+
 });
+
+$(".about").hide();
+
+
 
 $(".options").hover(function(){
 	$(this).addClass("show-gif");
 }, function(){
 	$(this).removeClass("show-gif");
 });
+
+$(".options2").hover(function(){
+  $(this).addClass("show-gif2");
+}, function(){
+  $(this).removeClass("show-gif2");
+});
+
+$(".options3").hover(function(){
+  $(this).addClass("show-gif3");
+}, function(){
+  $(this).removeClass("show-gif3");
+});
+
+$(".mid-buttons3").mouseover(function(){
+    $(".about").show();
+});
+$(".mid-buttons3").mouseout(function(){
+    $(".about").hide();
+});
+
+
   
 
 });
